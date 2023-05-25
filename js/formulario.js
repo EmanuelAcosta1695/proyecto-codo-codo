ec4264f69d447ff7be2c888e1d3aaae4236a4496
Emanuel Acosta <e.acosta1695@gmail.com>
2023-05-25 10:12:41 -0300

    Cambios esteticos del proyecto. Agrego api comentarios

--- js/formulario.js
+++ js/formulario.js
@@ -4,13 +4,23 @@ document.addEventListener('DOMContentLoaded', function() {
   form.addEventListener('submit', function(event) {
     event.preventDefault();
 
+
+    // Validar los datos ingresados por el usuario
+    if (nombre === '' || apellido === '' || email === '' || edad === '') {
+      alert('Por favor, complete todos los campos obligatorios.');
+      return;
+    }
+
+    if (isNaN(edad)) {
+      alert('La edad debe ser un número.');
+      return;
+    }
+  
     // Personaliza la dirección de correo electrónico en el atributo action del formulario
     // email encriptado
     const actionUrl = 'https://formsubmit.co/353e60ac6721fb40a1f21688c2050697';
 
-    // Realiza alguna acción con los datos del formulario, como enviar una solicitud AJAX
 
-    // Ejemplo de solicitud AJAX con fetch
     // Envio formulario con datos del user
     fetch(actionUrl, {
       method: 'POST',
@@ -18,19 +28,21 @@ document.addEventListener('DOMContentLoaded', function() {
     })
       .then(function(response) {
         if (response.ok) {
-          console.log('El formulario se envió correctamente');
-          // Realiza alguna acción adicional después de enviar el formulario
 
-          // Por ejemplo, puedes redirigir al usuario a otra página
+          console.log('El formulario se envió correctamente');
+
           window.location.href = 'http://localhost:5500/Proyecto-Codo-a-Codo/html/index.html';
+
         } else {
+
           console.error('Error al enviar el formulario');
-          // Realiza alguna acción adicional en caso de error
+    
         }
       })
       .catch(function(error) {
+
         console.error('Error en la solicitud:', error);
-        // Realiza alguna acción adicional en caso de error
+        
       });
   });
 });
